656c0d91081d5582564657ecf7075e679548026d
Om Bilthere <ombilthere953@gmail.com>
2026-02-06 20:34:39 +0530

    custom API Error  handled

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 // require('dotenv').config({path: './env'})
 
 import dotenv from 'dotenv'
-
-
-
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 })
 
+let port = process.env.PORT || 8000;
+
+
+
 connectDB()
+
+.then(()=>{
+
+    app.listen(port , ()=>{
+
+       console.log(`server is running on : ${port}`) 
+
+    })
+
+})
+.catch((err)=>{
+    console.log("MongoConnection failed" , err)
+})
 
 
 
